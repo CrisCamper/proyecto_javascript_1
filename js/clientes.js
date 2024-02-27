9ce556c838217b906d13682d7797067df4089496
Cristopher Daniel Ramirez Buitrago <145687056+CrisCamper@users.noreply.github.com>
2024-02-27 16:32:53 -0500

    Update clientes.js

--- js/clientes.js
+++ js/clientes.js
@@ -1,20 +1,51 @@
-const listaClientes = [];
+const listaClientes=[];
 
-const cargarClientes = () => {
-    for (let i = 0; i <= 10; i++) {
-        const nuevoCliente = {
-            id: i,
-            nombre: faker.name.findName(),
-            edad: Math.floor(Math.random * 30) + 18,
-            email: faker.internet.email()
-        };
-        listaClientes.push(nuevoCliente);
+
+const loadClientes= async()=>{
+   
+    try{
+        listaClientes.length=0;
+        const respuesta=await fetch('http://localhost:3000/clientes');
+
+        if(!respuesta.ok){
+           throw new Error('Error al cargar clientes. Estado: ',respuesta.status);
+        }
+        const clientes=await respuesta.json();
+        listaClientes.push(...clientes);
+
+    }catch(error){
+        console.error("Error al cargar clientes",error.message);
     }
 }
 
-const cargarFormularioClientes = () => {
-    const clientesForm = document.getElementById('clientes-form');
-    clientesForm.innerHTML = `
+
+const guardarCliente= async(nuevoCliente)=>{
+    try{
+
+        const respuesta=await fetch('http://localhost:3000/clientes',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(nuevoCliente),
+        });
+
+        if(!respuesta.ok){
+           throw new Error('Error al crear el cliente. Estado: ',respuesta.status);
+        }
+        const clienteCreado=await respuesta.json();
+       
+        
+        console.log('Cliente creado:', clienteCreado);
+
+    }catch(error){
+        console.error("Error al cargar clientes",error.message);
+    }
+}
+
+const cargarFormularioClientes=()=>{
+      const clientesForm = document.getElementById('clientes-form');
+      clientesForm.innerHTML = `
         <form>
             <label for="nombreCliente">Nombre del Cliente:</label>
             <input type="text" id="nombreCliente" required>
@@ -28,74 +59,77 @@ const cargarFormularioClientes = () => {
         </form>
     `;
     const listadoClientes = document.getElementById('listado-clientes');
-    listadoClientes.style.display = 'none';
+    listadoClientes.style.display='none';
 }
 
-const crearCliente = () => {
-    const nombreInput = document.getElementById('nombreCliente');
-    const edadInput = document.getElementById('edadCliente');
-    const emailInput = document.getElementById('emailCliente');
+const crearCliente= async ()=>{
+    const nombreInput=document.getElementById('nombreCliente');
+    const edadInput=document.getElementById('edadCliente');
+    const emailInput=document.getElementById('emailCliente');
 
-    const nombre = nombreInput.value;
-    const edad = edadInput.value;
-    const email = emailInput.value;
+    const nombre=nombreInput.value;
+    const edad=edadInput.value;
+    const email=emailInput.value;
 
-    const nuevoCliente = {
-        id: listaClientes.length + 1,
-        nombre: nombre,
+    const nuevoCliente={
+        id:listaClientes.length+1,
+        nombre:nombre,
         edad: edad,
         email: email
     }
 
-    listaClientes.push(nuevoCliente);
-
-    nombreInput.value = '';
-    edadInput.value = '';
-    emailInput.value = '';
+  
+    await guardarCliente(nuevoCliente);
+    await loadClientes();
+   
+    nombreInput.value='';
+    edadInput.value='';
+    emailInput.value='';
 
     alert('Cliente creado con éxito!');
-    console.log(listaClientes);
-
+    
     actulizarClientesEnFacturas();
 
     return nuevoCliente;
 
 }
 
-const mostrarListado = () => {
+const mostrarListado= async ()=>{
+    await loadClientes();
     const clientesForm = document.getElementById('clientes-form');
     const listadoClientes = document.getElementById('listado-clientes');
+    
+    clientesForm.style.display='none';
+    listadoClientes.style.display='block';
 
-    clientesForm.style.display = 'none';
-    listadoClientes.style.display = 'block';
+    const ul=document.createElement('ul');
 
-    const ul = document.createElement('ul');
-
-    for (const cliente of listaClientes) {
-        const li = document.createElement('li');
-        li.textContent = `ID: ${cliente.id}, Nombre: ${cliente.nombre}, Edad: ${cliente.edad}, Email: ${cliente.email}`;
+    for(const cliente of listaClientes){
+        const li=document.createElement('li');
+        li.textContent= `ID: ${cliente.id}, Nombre: ${cliente.nombre}, Edad: ${cliente.edad}, Email: ${cliente.email}`;
         ul.appendChild(li);
     }
 
-    listadoClientes.innerHTML = '';
+    listadoClientes.innerHTML='';
     listadoClientes.appendChild(ul);
 
-    const volverButton = document.createElement('button');
-    volverButton.textContent = 'Volver al Formulario';
-    volverButton.addEventListener('click', volverFormulario);
+    const volverButton=document.createElement('button');
+    volverButton.textContent='Volver al Formulario';
+    volverButton.addEventListener('click',volverFormulario);
     listadoClientes.appendChild(volverButton);
-
+    
 }
 
-const volverFormulario = () => {
-    const clientesForm = document.getElementById('clientes-form');
+const volverFormulario=()=>{
+    const clientesForm=document.getElementById('clientes-form');
     const listadoClientes = document.getElementById('listado-clientes');
 
-    listadoClientes.style.display = 'none';
-    clientesForm.style.display = 'block';
-
+    listadoClientes.style.display='none';
+    clientesForm.style.display='block';
+    
 }
 
 
 
-console.log(listaClientes); 
+
+console.log(listaClientes);
